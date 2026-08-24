448b77fc8575cba8531b26a07e218e4c2daf1714
redmiemre02-a11y <redmiemre02@gmail.com>
2026-08-24 12:18:12 +0300

    🔧 fix: upgrade all slug logic to support Astro 5 glob loader IDs

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -14,7 +15,7 @@ const blog = defineCollection({
     tags: z.array(z.string()).default([]),
     author: z.string().default('Experto Tech'),
     readingTime: z.number().optional(),
-  }),
+  })
 });
 
 export const collections = { blog };
